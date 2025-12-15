ecd8cf2f2dd39fe93e0b9c68fe793fa2a1eb628d
Arafat Rahman <65125880+Rusian007@users.noreply.github.com>
2025-12-15 11:43:51 +0600

    Add files via upload

---
+++ src/apnProvider.js
@@ -0,0 +1,18 @@
+// apnProvider.js or at top of your file
+const apn = require('apn');
+const path = require('path');
+const appleAPNKey = require(path.join(__dirname, '../firebase/apple_key.json'));
+
+var options = {
+
+    token: {
+      key: path.join(__dirname, '../firebase/AuthKey.p8'),
+      keyId: appleAPNKey.keyid,
+      teamId: appleAPNKey.teamid
+    },
+    production: false
+  };
+
+const apnProvider = new apn.Provider(options);
+
+module.exports = apnProvider;
